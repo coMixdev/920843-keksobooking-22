c6fe9e76e07cb7869a0c8e677fc9509af8b7b478
Tim <comixtim@gmail.com>
2021-02-01 12:14:47 +0300

    Исправление

--- js/main.js
+++ js/main.js
@@ -1,30 +1,26 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно
 
-const getRandomNum = function (min, max) {
-  if (min < max) {
-    if (min > 0 && max > 0) {
-      return Math.round(Math.random() * (max - min) + min);
-    } else {
-      return ('Введите положительные числа');
-    }
+const getRandomNum = (min, max) => {
+  if (min <= max) {
+    return Math.round(Math.random() * (max - min) + min);
   } else {
-    return('Первое число должно быть больше второго');
+    throw ('Первое число должно быть меньше второго')
+
   }
 };
+
 
 getRandomNum()
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#получение_случайного_числа_в_заданном_интервале
 
 
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
-const getFloatNum = function (min, max, float) {
-  if(min < max) {
-    if (min > 0 && max > 0){
-      const result = (Math.random() * (max - min) + min);
-      return `${result.toFixed(float)}, количество знаков после запятой: ${float}`;
-    } else { return('Введите положительные числа')
-    }
-  } else {return('Первое число должно быть больше второго')
+const getFloatNum = (min, max, float) => {
+  if(min <= max) {
+    const result = (Math.random() * (max - min) + min);
+    return `${result.toFixed(float)}, количество знаков после запятой: ${float}`;
+  } else {
+    throw ('Первое число должно быть меньше второго')
   }
 }
 
