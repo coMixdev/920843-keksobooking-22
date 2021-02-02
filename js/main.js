e067bbc292d42c1eadf228fe33ee8d8b050791ac
Tim <comixtim@gmail.com>
2021-02-02 19:38:01 +0300

    Исправление

--- js/main.js
+++ js/main.js
@@ -1,27 +1,28 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно
 
 const getRandomNum = (min, max) => {
-  if (min <= max) {
+  if (min > max){
+    throw new Error('Первое число должно быть меньше второго');
+  }
+  else {
     return Math.round(Math.random() * (max - min) + min);
-  } else {
-    throw ('Первое число должно быть меньше второго')
-
   }
 };
 
 
-getRandomNum()
+getRandomNum();
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#получение_случайного_числа_в_заданном_интервале
 
 
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
 const getFloatNum = (min, max, float) => {
-  if(min <= max) {
-    const result = (Math.random() * (max - min) + min);
-    return `${result.toFixed(float)}, количество знаков после запятой: ${float}`;
-  } else {
-    throw ('Первое число должно быть меньше второго')
+  if (min > max){
+    throw new Error('Первое число должно быть меньше второго');
+  }
+  else {
+    const result = Math.random() * (max - min) + min;
+    return result.toFixed(float);
   }
 }
 
-getFloatNum()
+getFloatNum();
