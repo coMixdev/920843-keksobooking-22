e8b1bb3d674af4c9b6516289da3fe02793edfd4a
Tim <comixtim@gmail.com>
2021-02-13 21:55:55 +0300

    исправление 3

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@
 
 const TYPES = ['palace', 'flat', 'house', 'bungalo'];
 const TIMES = ['12:00', '13:00', '14:00'];
+const NUMBERS_DESCRIPTION = ['Номер класса делюкс с гидромассажной ванной', 'Стандартный номер с общей ванной', 'Номер с видом на сад'];
 const OPTIONS = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const PHOTOS_ADRESS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 const ADS_COUNT = 10;
 const AVATAR_COUNT = 8;
+const highestPrice = 10000;
 
 
 //Функция, возвращающая случайное целое число из переданного диапазона включительно
@@ -29,7 +31,7 @@ const getFloatNum = (min, max, float) => {
   }
   const result = Math.random() * (max - min) + min;
   return result.toFixed(float);
-}
+};
 
 
 //Функция для расчета номера изображения
@@ -44,59 +46,59 @@ const getRandomElement = (items) => {
   const index = getRandomNum(0, items.length-1);
   const item = items[index];
   return item;
-}
+};
 
 //Функция для перетасовки массива
 
-const shuffle = (list) => {
+const getShuffled = (list) => {
   for (let i = list.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
+    const j = Math.floor(Math.random() * (i + 1));
     [list[i], list[j]] = [list[j], list[i]];
   }
   return list;
-}
+};
 
 //Функция для получения массива случайной длины
 
-const getRandomItem = (list) => {
-  let shuffled = shuffle(list);
+const getRandomArrayLength = (list) => {
+  let shuffled = getShuffled(list);
   let listLength = getRandomNum(1, list.length);
   let newList = shuffled.slice(0, listLength);
   return newList;
-}
+};
 
 
 const createAdvertising = () => {
-  const LOCATION_X = getFloatNum(35.65000, 35.70000, 5);
-  const LOCATION_Y = getFloatNum(139.70000, 139.80000, 5);
+  const locationX = getFloatNum(35.65000, 35.70000, 5);
+  const locationY = getFloatNum(139.70000, 139.80000, 5);
   return {
     author : {
       avatar: getAvatarUrl(),
     },
     offer : {
       title: 'Самое лучшее предложение',
-      adress: `${LOCATION_X}, ${LOCATION_Y}`,
-      price: getRandomNum(1, 100000),
+      adress: `${locationX}, ${locationY}`,
+      price: getRandomNum(1, highestPrice),
       type : getRandomElement(TYPES),
       rooms : getRandomNum(1, 8),
       guests : getRandomNum(1, 15),
       checkin : getRandomElement(TIMES),
       checkout : getRandomElement(TIMES),
-      features : getRandomItem(OPTIONS),
-      description : 'Номер класса делюкс с гидромассажной ванной',
-      photos : getRandomItem(PHOTOS_ADRESS),
+      features : getRandomArrayLength(OPTIONS),
+      description : getRandomArrayLength(NUMBERS_DESCRIPTION),
+      photos : getRandomArrayLength(PHOTOS_ADRESS),
     },
     location : {
-      x : LOCATION_X,
-      y : LOCATION_Y,
+      x : locationX,
+      y : locationY,
 
     },
   }
-}
+};
 
 const ads = (adsNumber) => {
   const advert = new Array(adsNumber).fill(null).map(createAdvertising);
   return advert;
-}
+};
 
 ads(ADS_COUNT);
